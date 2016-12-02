8d0e5d6739b2b855bfc704689c8352f71fdbef09
JadziaHax <madelyn.a.thomas@gmail.com>
2016-12-02 10:41:27 -0500

    Design Puppy Class

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+=begin
+BREED -----------------------------
+Weimaraner
+
+CHARACTERISTICS -------------------
+Eyes: sad
+Lonely: yes
+Leg count: 4
+Name: varies
+Color: silver-grey
+
+BEHAVIOR --------------------------
+Lick
+Follow
+Shame
+=end
