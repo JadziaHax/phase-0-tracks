f387908600ad9ada5c79f43133f269d73175350f
JadziaHax <madelyn.a.thomas@gmail.com>
2016-12-12 00:00:29 -0500

    Psuedocode the first release.

---
+++ js/algos.js
@@ -0,0 +1,13 @@
+//Relase 0
+//Set a "current high" variable equal to 0
+//Define a function that takes an array of strings as a parameter and in that function:
+//  Start a loop that iterates through the array.
+//    For each string in the array evaluate the length.
+//    if the length is greater than the value of "current high":
+//      store the index position of the string in a variable called "winner"
+//  end the loop
+//  print the string in the index position of the value of "winner" from array.
+
+function (array) {
+
+}
