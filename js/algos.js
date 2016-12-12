33ada5914a0d8892e076a7481b1ddc3ad8d4ec7c
JadziaHax <madelyn.a.thomas@gmail.com>
2016-12-12 00:55:23 -0500

    Psuedocode and Code the second release.

--- js/algos.js
+++ js/algos.js
@@ -21,6 +21,44 @@ function longestString(array) {
   return array[winner];
 }
 
-longestString(["long phrase","longest phrase","longer phrase"]);
-longestString(["left","right","forward","backward"]);
-longestString(["cat","goose","elephant"]);
+//Release 1
+//Define a function that takes two objects as parameter and in that function:
+//  define variable match with the value false.
+//  Start a loop that iterates through the first object.
+//    For each key in the first object, start a loop that iterates through the second object.
+//      For each key in the second object, compare it to the key of the current iteration of the first object.
+//      If the keys match
+//        Compare their values
+//        If the values match
+//          set match to true
+//      end the loop
+//  end the loop
+//  return variable match
+
+function obj_compare(obj1, obj2) {
+  var match = false;
+  for (var key_1 in obj1) {
+    for (var key_2 in obj2) {
+      if (key_2 == key_1) {
+        if (obj1[key_1] == obj2[key_2]) {
+          match = true;
+        }
+      }
+    }
+  }
+  return match;
+}
+
+//Release 0 driver code
+console.log(longestString(["long phrase","longest phrase","longer phrase"]));
+console.log(longestString(["left","right","forward","backward"]));
+console.log(longestString(["cat","goose","elephant"]));
+
+//Release 1 driver code
+var dogs_1 = {bill: "grey", jill: "brown"};
+var dogs_2 = {jake: "yellow", bill: "grey"};
+var dogs_3 = {lula: "green", jake: "yellow"};
+
+console.log(obj_compare(dogs_1, dogs_2));
+console.log(obj_compare(dogs_1, dogs_3));
+console.log(obj_compare(dogs_2, dogs_3));
