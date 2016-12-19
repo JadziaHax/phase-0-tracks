2360d78dea30e73de62e4635dd81aa0f82902084
JadziaHax <madelyn.a.thomas@gmail.com>
2016-12-19 08:29:50 -0800

    Write driver code for and debug function for adding items to table

--- databases/8_5/todo.rb
+++ databases/8_5/todo.rb
@@ -16,3 +16,13 @@ SQL
 def add_item(db, task, deadline)
   db.execute('INSERT INTO todo (task, deadline, complete) VALUES (?, ?, "false")', [task, deadline])
 end
+
+puts "Please enter a task"
+user_task = gets.chomp
+puts "Please enter a deadline date in the following format: DD-MM-YYYY"
+user_deadline = gets.chomp
+add_item(db, user_task, user_deadline)
+tasks = db.execute("SELECT * FROM todo")
+tasks.each do |task|
+  puts "#{task[1]} is due by #{task[2]}"
+end
