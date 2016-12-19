52380be3b9985f008e885e8a62d4c49faabc0144
JadziaHax <madelyn.a.thomas@gmail.com>
2016-12-19 08:20:18 -0800

    Start ruby program for todo list by creating code to create table in sqlite.

---
+++ databases/8_5/todo.rb
@@ -0,0 +1,13 @@
+require 'sqlite3'
+require 'faker'
+
+db = SQLite3::Database.new("todo.db")
+
+db.execute = <<-SQL
+  CREATE TABLE IF NOT EXISTS todo(
+    id INTEGER PRIMARY KEY,
+    task VARCHAR(255),
+    deadline VARCHAR(10)
+    complete boolean
+  )
+SQL
