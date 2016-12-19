3435f21cfb7ac9ba20add1c472c27ab73c8d6d3b
JadziaHax <madelyn.a.thomas@gmail.com>
2016-12-19 08:31:56 -0800

    Write function to return an item based on its primary key id.

--- databases/8_5/todo.rb
+++ databases/8_5/todo.rb
@@ -17,6 +17,12 @@ def add_item(db, task, deadline)
   db.execute('INSERT INTO todo (task, deadline, complete) VALUES (?, ?, "false")', [task, deadline])
 end
 
+def return_item(db, id)
+  db.execute('SELECT * FROM todo WHERE id=?', [id])
+end
+
+=begin
+#add_item driver code
 puts "Please enter a task"
 user_task = gets.chomp
 puts "Please enter a deadline date in the following format: DD-MM-YYYY"
@@ -26,3 +32,4 @@ tasks = db.execute("SELECT * FROM todo")
 tasks.each do |task|
   puts "#{task[1]} is due by #{task[2]}"
 end
+=end
