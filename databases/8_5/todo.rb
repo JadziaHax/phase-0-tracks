c2979e93ff35da7f69d1d4768e97978243d5933a
JadziaHax <madelyn.a.thomas@gmail.com>
2016-12-19 08:22:31 -0800

    Debugged database and table creation.

--- databases/8_5/todo.rb
+++ databases/8_5/todo.rb
@@ -3,11 +3,12 @@ require 'faker'
 
 db = SQLite3::Database.new("todo.db")
 
-db.execute = <<-SQL
+db.execute(<<-SQL
   CREATE TABLE IF NOT EXISTS todo(
     id INTEGER PRIMARY KEY,
     task VARCHAR(255),
-    deadline VARCHAR(10)
+    deadline VARCHAR(10),
     complete boolean
   )
 SQL
+)
