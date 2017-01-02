c40d1797d7ac4f877505a7cb523ca2b0439b1a0b
JadziaHax <madelyn.a.thomas@gmail.com>
2017-01-02 03:52:29 -0500

    Updated with code from pair.

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -7,20 +7,37 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
+db.execute(<<-SQL
+  CREATE TABLE IF NOT EXISTS campuses(
+    id INTEGER PRIMARY KEY,
+    city VARCHAR(255)
+  )
+SQL
+)
+
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
-  erb :home
+  @campuses = db.execute("SELECT * FROM campuses")
+  erb :new_home
 end
 
 get '/students/new' do
   erb :new_student
 end
 
+get '/campuses/new' do
+  erb :campuses
+end
 # create new students via
 # a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
+  redirect '/'
+end
+
+post '/campuses' do
+  db.execute("INSERT INTO campuses (city) VALUES (?)", [params['city']])
   redirect '/'
 end
 
